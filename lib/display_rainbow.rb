8d45c7dbaf40ea49a01a554712889478baae8e20
Ted Barlow <ted@noukod.com>
2018-08-30 15:39:13 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(color1, color2, color3, color4, color5, color6, color7)
+  puts "R: #{color1} O: #{color2 }Y: #{color3} G: #{color4} B: #{color5} I: #{color6} V: #{color7}"
+end
